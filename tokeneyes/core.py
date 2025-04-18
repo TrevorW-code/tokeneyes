3a5763065bb99a080aee2ef2a368905889cd9f0e
Trevor <trevorward001@gmail.com>
2025-04-18 14:58:32 -0600

    style: removed print

--- tokeneyes/core.py
+++ tokeneyes/core.py
@@ -2,7 +2,6 @@ from transformers import AutoTokenizer
 
 def get_model_name_from_tokenizer(tokenizer: AutoTokenizer):
     if hasattr(tokenizer, "name_or_path"):
-        print('path')
         return tokenizer.name_or_path
     elif hasattr(tokenizer, "init_kwargs") and "name_or_path" in tokenizer.init_kwargs:
         return tokenizer.init_kwargs["name_or_path"]
