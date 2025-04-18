8eeb42c6b773ebf0c6ee5d1ca43a87c28946b3ba
Trevor <trevorward001@gmail.com>
2025-04-18 15:01:01 -0600

    feat(widget): added token id + raw token to hover
    - Renamed `parsed_text_visualization` -> `input_vis`
    - Added span hover to provide token id + raw token
    - Implemented `input_vis` as single dispatch
        - allows me to extend later with audio possibly

--- tokeneyes/widget.py
+++ tokeneyes/widget.py
@@ -1,13 +1,10 @@
 from IPython.display import HTML, display
 from transformers import AutoTokenizer
 from tokeneyes.colors import get_index_colors
+from tokeneyes.core import get_model_name_from_tokenizer
 from tokeneyes.web import CSS, make_token_span, make_info_line
-
-# def raw_token_visualization(text: str, tokz: AutoTokenizer):
-#     tokens = tokz.tokenize(text)
-#     token_colors = get_token_colors(text, tokz)
-#     tokens_vis = make_colored_tokens_html(tokens, token_colors)
-#     return tokens_vis, tokens
+from typing import Callable, Any
+import functools as ft
 
 def make_colored_tokens_html(tokens, token_colors):
     colored_text = ""
@@ -21,33 +18,50 @@ def make_colored_tokens_html(tokens, token_colors):
         colored_text += span
     return colored_text
 
-def parsed_text_visualization(text: str, tokz: AutoTokenizer, dark_mode: bool):
+@ft.singledispatch
+def input_vis(inp: Any, tokz: AutoTokenizer, dark_mode: bool, color_by: Callable):
+    raise ValueError(f'No Visualization for Input Type: {type(inp)}')
+
+@input_vis.register
+def _(inp: str, tokz: AutoTokenizer, dark_mode: bool, color_by: Callable):
     """
     Visualizes how input text is parsed, rather than displaying the encoded actual string
     """
-    encoding = tokz(text, return_offsets_mapping=True)
-    tokens = tokz.tokenize(text)
+    
+    encoding = tokz(inp, return_offsets_mapping=True)
+    tokens = tokz.tokenize(inp)
     index_tuples, ids = encoding['offset_mapping'], encoding['input_ids']
     index_colors = get_index_colors(index_tuples, dark_mode)
+    id2token = {id: tokz.convert_ids_to_tokens(id) for id in encoding['input_ids']}
     
+    # write spans
     spans = ""
     for idx, tup in enumerate(index_tuples):
-        s,e = tup
-        if e - s > 0:
+        start,end = tup
+        if end - start > 0:
+            current_id = ids[idx]
             spans += make_token_span(
                 color = index_colors[tup],
-                hover = ids[idx],
-                content = text[s:e]
+                hover = f'id: {current_id}\nraw: {id2token[current_id]}',
+                content = inp[start:end]
             )
-
     return spans, tokens
 
-def show_tokens(text: str, model_str: str, dark_mode: bool = False):
-    tokz = AutoTokenizer.from_pretrained(model_str)
-    model = "<p>" + make_info_line("model", model_str) + "</p>"
-    tokens_vis, tokens = parsed_text_visualization(text,tokz, dark_mode=dark_mode)
-    # tokens_vis, tokens = raw_token_visualization(text, tokz)
+def show_tokens(text: str, tokenizer: AutoTokenizer | str, dark_mode: bool = False, color_by = get_index_colors):
+    # get tokenizer
+    if isinstance(tokenizer, str):
+        model = tokenizer
+        tokz = AutoTokenizer.from_pretrained(tokenizer)
+    else:
+        model = get_model_name_from_tokenizer(tokenizer)
+        tokz = tokenizer
+    
+    # visualize tokens
+    tokens_vis, tokens = input_vis(text, tokz, dark_mode=dark_mode, color_by = color_by)
     num_tokens, num_chars = len(tokens), len(text)
+    
+    # make info
+    model = "<p>" + make_info_line("model", model) + "</p>"
     token_info = make_info_line("tokens", num_tokens)
     char_info = make_info_line("chars", num_chars)
     counts = "<p>" + token_info + ", " + char_info + "</p>"
